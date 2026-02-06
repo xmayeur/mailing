4d4ec8706817d4d2b0f12df17547aabb9adc3b95
xmayeur <xavier@mayeur.be>
2026-02-06 15:14:50 +0100

    Switch `Mock` to `MagicMock` in email filter tests, update row structure, extend indices, and refine SMTP condition in `sendMail.py`.

--- sendMail.py
+++ sendMail.py
@@ -709,7 +709,7 @@ def generate_mailing(param):
             if not param.donotsend:
                 msg, recipients = build_email(param=param, subject=param.subject, message=msg_body,
                                   bcc=",".join(addressees), attachments=param.file)
-                if param.profile == 'artscroises':
+                if hasattr(param, 'smtp_host'):    # param.profile == 'artscroises':
                     send_mail(param=param, message=msg, recipients=recipients)
                 elif param.profile == 'cambristi':
                     send_gmail(get_gmail_service(param), message=msg)
